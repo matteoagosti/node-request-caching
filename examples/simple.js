d43bc8df699c9039225afec9f91dc6e34465d010
Matteo Agosti <matteo.agosti@gmail.com>
2012-09-06 16:32:34 +0200

    Simple example commented

--- examples/simple.js
+++ examples/simple.js
@@ -1,12 +1,19 @@
 var RequestCaching = require('../lib/request-caching');
+
+// Cache into Memory
 var rc = new RequestCaching();
 
 for (var i = 0; i < 10; i++) {
   setTimeout(function() {
-    rc.get('https://graph.facebook.com/facebook', {fields: 'id,name'}, 1, function(err, res, body, cache) {
-      console.log('Response', res);
-      console.log('Body', body);
-      console.log('Cache', cache);
-    });
+    rc.get(
+      'https://graph.facebook.com/facebook',  // URI
+      {fields: 'id,name'},                    // query string params
+      1,                                      // TTL in seconds
+      function(err, res, body, cache) {
+        console.log('Response', res);         // response params object (headers, statusCode, ...)
+        console.log('Body', body);            // response body as string
+        console.log('Cache', cache);          // cache info object (hit, key)
+      }
+    );
   }, i * 1000);
 }
