8b522e6ab6d99a45c72052f3ac74fd868921173d
Matteo Agosti <matteo.agosti@gmail.com>
2012-09-06 14:42:47 +0200

    Linting

--- examples/simple.js
+++ examples/simple.js
@@ -1,12 +1,12 @@
 var RequestCaching = require('../lib/request-caching');
 var rc = new RequestCaching();
 
-for(var i=0; i<10; i++) {
+for (var i = 0; i < 10; i++) {
   setTimeout(function() {
     rc.get('https://graph.facebook.com/facebook', {fields: 'id,name'}, 1, function(err, res, body, cache) {
       console.log('Response', res);
       console.log('Body', body);
       console.log('Cache', cache);
     });
-  }, i*1000);
+  }, i * 1000);
 }
